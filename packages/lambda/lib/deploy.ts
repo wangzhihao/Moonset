319d14f83304d668c7c75b9a9ffc1f1a2f3ac178
Zhihao Wang <accept.acm@gmail.com>
2020-07-22 07:56:57 +0000

    foo

--- packages/lambda/lib/deploy.ts
+++ packages/lambda/lib/deploy.ts
@@ -1,9 +1,7 @@
-import {v4 as uuid} from 'uuid';
 import {MoonsetConstants as MC} from '@moonset/executor';
 import {Config, ConfigConstant as CC, logger, Serde} from '@moonset/util';
 import {CommonConstants as MCC} from '@moonset/util';
 import {ISDK, SDKProvider, CDKApp} from '@moonset/util';
-import * as execa from 'execa';
 import * as path from 'path';
 
 export class Deploy{
